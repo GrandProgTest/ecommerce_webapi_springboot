40a1bffb8d192b3244e8961883548b655ceb563b
GrandProgTest <u202124343@upc.edu.pe>
2026-01-28 10:23:23 -0500

    feat: added deleteusercommand and role repository into iam bc

---
+++ src/main/java/com/finalproject/ecommerce/ecommerce/iam/infrastructure/persistence/jpa/repositories/RoleRepository.java
@@ -0,0 +1,14 @@
+package com.finalproject.ecommerce.ecommerce.iam.infrastructure.persistence.jpa.repositories;
+
+import com.finalproject.ecommerce.ecommerce.iam.domain.model.entities.Role;
+import com.finalproject.ecommerce.ecommerce.iam.domain.model.valueobjects.Roles;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(Roles name);
+    boolean existsByName(Roles name);
+}
