e8a19f4765f36bed6694d623d27b6fe65928c511
GrandProgTest <u202124343@upc.edu.pe>
2026-02-02 12:56:59 -0500

    fix: modified classes for consistency issues (products bc)

--- src/main/java/com/finalproject/ecommerce/ecommerce/products/domain/model/commands/DeleteProductCommand.java
+++ src/main/java/com/finalproject/ecommerce/ecommerce/products/domain/model/commands/DeleteProductCommand.java
@@ -1,6 +1,8 @@
 package com.finalproject.ecommerce.ecommerce.products.domain.model.commands;
 
-public record DeleteProductCommand (Long productId) {
+public record DeleteProductCommand (
+        Long productId
+) {
     public DeleteProductCommand{
         if(productId == null || productId <= 0){
             throw new IllegalArgumentException("productId cannot be null or less than 1");
